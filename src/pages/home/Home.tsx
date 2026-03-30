b7a7e5858c9f50b3f65e3483b124b17fcf78aad9
LaraBatista04 <lara.batista@fatec.sp.gov.br>
2026-03-30 16:47:57 -0300

    Adicionar novas imagens de solução e atualizar o componente Card com suporte a imagens, além de melhorias na estilização da página inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <section className='home'>
       <div className='sectionHome'>
-        <div className='main'>
+        <div className='contentHome'>
           <h2 className='titleHome'>
             Tem um desafio digital?
             <br />
@@ -42,7 +42,7 @@ const Home = () => {
       <div className='cards'>
         <Card
           view='primario'
-          height={'78%'}
+          height={'140px'}
           title='Aplicativos Mobile'
           text='Criamos experiências nativas e híbridas de alta performance,
           focadas em usabilidade e prontas para escalar nas lojas iOS e Android.'
@@ -50,7 +50,7 @@ const Home = () => {
         />
         <Card
           view='primario'
-          height={'78%'}
+          height={'140px'}
           title='Sistemas Internos'
           text='Automatize processos e ganhe produtividade com softwares de 
           gestão personalizados, feitos sob medida para a sua operação.'
@@ -58,7 +58,7 @@ const Home = () => {
         />
         <Card
           view='primario'
-          height={'78%'}
+          height={'140px'}
           title='Sites de Alta Conversão'
           text='Mais que um cartão de visitas: desenvolvemos portais e sites institucionais 
           otimizados para busca (SEO) e focados em gerar novos negócios.'
@@ -66,14 +66,54 @@ const Home = () => {
         />
         <Card
           view='primario'
-          height={'78%'}
+          height={'140px'}
           title='Inteligência Artificial'
           text='Implementamos IA para análise de dados, automação de tarefas e chatbots
           inteligentes que elevam o patamar tecnológico da sua empresa.'
           icon={<BotMessageSquare />}
         />
       </div>
-
+      <div className='solutions'>
+        <h2 className='titleSolution'>Soluções</h2>
+        <div className='contentSolutions'>
+          <Card
+            view="secundario"
+            image="/solution1.jpg"
+            title="Site Institucional"
+            text='Desenvolvemos portais robustos, seguros e com alta performance para sua empresa. 
+            Conteúdo gerenciável, UX intuitiva e conformidade com todas as diretrizes da LGPD.'
+            width={'300px'}
+            height={'420px'}
+          />
+          <Card
+            view="secundario"
+            image="/solution2.jpg"
+            title="Sistemas & Plataformas"
+            text='Engenharia de arquiteturas web escaláveis (ERPs, CRMs, Intranets e Portais B2B/B2C)
+            com foco em segurança, integração de processos e performance de alto tráfego.'
+            width={'300px'}
+            height={'420px'}
+          />
+          <Card
+            view="secundario"
+            image="/solution3.jpg"
+            title="Aplicativo Mobile"
+            text='Projetamos e desenvolvemos aplicativos nativos e híbridos com design focado no usuário, 
+            performance otimizada e integração perfeita para as plataformas iOS e Android.'
+            width={'300px'}
+            height={'420px'}
+          />
+          <Card
+            view="secundario"
+            image="/solution4.jpg"
+            title="Automação Inteligente"
+            text='Projetamos e desenvolvemos aplicativos nativos e híbridos com design focado no usuário,
+            performance otimizada e integração perfeita para as plataformas iOS e Android.'
+            width={'300px'}
+            height={'420px'}
+          />
+        </div>
+      </div>
     </section>
   );
 };
