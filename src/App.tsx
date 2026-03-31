53dfccd2fd1ae2a38382ced07360600273edb578
MacroZero0 <ebarreto375@gmail.com>
2026-03-31 17:43:57 -0300

    ajustes visuais no carrossel e contact

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
           element={
             <>
               <SolutionDetail />
+              <Contact />
             </>
           }
         />
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,6 +4,7 @@ import Button from '../button/Button';
 const Contact = () => {
     return (
         <section className='contact' id='contato'>
+            <div className='contact-overlay'></div>
             <div className='contactContent'>
                 <h2 className='contactTitle'>
                     Viabilizando a próxima<br /> era digital da sua operação.
