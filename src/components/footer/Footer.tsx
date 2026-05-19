99021e7283c0a84e0ef80f5c0e71dd8d14bbc8a9
Wozniak7 <danillo.soares@fatec.sp.gov.br>
2026-05-19 16:26:48 -0300

    Fix: corrigir visualmente o footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,60 +7,56 @@ const Footer = () => {
             <div className='footerContainer'>
 
                 <div className='footerTop'>
-                    <div className='footerBrand'>
-                        <Link to="/">
+                    <div className='footerBrandColumn'>
+                        <Link to="/" className='footerLogoLink'>
                             <img className='footerLogo' src="/logo-branco-copy.png" alt="Software House Logo" loading="lazy" decoding="async" />
                         </Link>
+                        <p className='footerBrandDescription'>
+                            Transformando ideias em soluções digitais inovadoras, criando o futuro da tecnologia com excelência.
+                        </p>
                     </div>
 
-                    <div className='footerContact'>
-                        <p>
-                            <img className='footerIcon' src="/icons/whatsapp.png" alt="WhatsApp" loading="lazy" decoding="async" />
-                            +55 (11) 4002-8922
-                        </p>
-                        <p>
-                            <img className='footerIcon' src="/icons/mail.png" alt="Email" loading="lazy" decoding="async" />
-                            fale@software.house
-                        </p>
-                        <div className='footerSocialIcons'>
-                            {/* <a className='footerSocialLink' href='#' aria-label="LinkedIn">
-                                <img src="/icons/linkedin.png" alt="LinkedIn" loading="lazy" decoding="async" />
-                            </a> */}
-                            <a className='footerSocialLink' href='https://www.instagram.com/softwarehouse.sigma' aria-label="Instagram">
-                                <img src="/icons/instagram.png" alt="Instagram" loading="lazy" decoding="async" />
+                    <div className='footerNavColumn'>
+                        <h4 className='footerColumnTitle'>Links Rápidos</h4>
+                        <nav className='footerNav'>
+                            <ul>
+                                <li><a href="/#solucoes">Soluções</a></li>
+                                <li><Link to="/showcases">Showcases</Link></li>
+                                <li><a href="/#setores">Setores</a></li>
+                                <li><a href="/#sobre">Sobre</a></li>
+                                <li><a href="/#contato">Contato</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div className='footerContactColumn'>
+                        <h4 className='footerColumnTitle'>Contato</h4>
+                        <div className='footerContactList'>
+                            <a href="https://wa.me/551140028922" className='footerContactItem' target="_blank" rel="noopener noreferrer">
+                                <img className='footerIcon' src="/icons/whatsapp.png" alt="WhatsApp" loading="lazy" decoding="async" />
+                                +55 (11) 4002-8922
                             </a>
-                            {/* <a className='footerSocialLink' href='#' aria-label="Facebook">
-                                <img src="/icons/facebook.png" alt="Facebook" loading="lazy" decoding="async" />
+                            <a href="mailto:fale@software.house" className='footerContactItem'>
+                                <img className='footerIcon' src="/icons/mail.png" alt="Email" loading="lazy" decoding="async" />
+                                fale@software.house
                             </a>
-                            <a className='footerSocialLink' href='#' aria-label="YouTube">
-                                <img src="/icons/youtube.png" alt="YouTube" loading="lazy" decoding="async" />
+                            <a href="https://www.instagram.com/softwarehouse.sigma" className='footerContactItem' target="_blank" rel="noopener noreferrer">
+                                <img className='footerIcon' src="/icons/instagram.png" alt="Instagram" loading="lazy" decoding="async" />
+                                Instagram
                             </a>
-                            <a className='footerSocialLink' href='#' aria-label="TikTok">
-                                <img src="/icons/tiktok.png" alt="TikTok" loading="lazy" decoding="async" />
-                            </a> */}
                         </div>
                     </div>
-
-                    <nav className='footerNav'>
-                        <ul>
-                            <li><a href="/#solucoes">Soluções</a></li>
-                            <li><Link to="/showcases">Showcases</Link></li>
-                            <li><a href="/#setores">Setores</a></li>
-                            <li><a href="/#sobre">Sobre</a></li>
-                            <li><a href="/#contato">Contato</a></li>
-                        </ul>
-                    </nav>
                 </div>
 
                 <div className='footerBottom'>
                     <div className='footerCopy'>
-                        <span>2026</span>
-                        <span>SOFTWARE HOUSE © Todos os direitos reservados.</span>
+                        <span>© {new Date().getFullYear()} Software House.</span>
+                        <span className='footerRights'>Todos os direitos reservados.</span>
                     </div>
                     <div className='footerLegalLinks'>
-                        <a href="#">Política de Cookies</a>
                         <a href="#">Política de Privacidade</a>
-                        <a href="#">Manual do Site</a>
+                        <a href="#">Termos de Uso</a>
+                        <a href="#">Política de Cookies</a>
                     </div>
                 </div>
 
